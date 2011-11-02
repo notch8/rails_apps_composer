44b05d782592a0b32abcc6b9db1e620c86d2d127
Andrew Dixon <dixon@atomicbroadcast.net>
2011-11-01 17:06:48 -0700

    working on devise_omniauth.

--- recipes/devise_omniauth.rb
+++ recipes/devise_omniauth.rb
@@ -4,9 +4,32 @@ github_url = 'https://github.com/spinlock99/rails_apps_composer/raw/master/'
 gem 'omniauth', '>= 0.3.0.rc3'
 
 after_bundler do
+  #
+  # Add omniauth.rb to initializers to configure 
+  # authentication providers for our middleware.
+  #
   inside 'config/initializers' do
-    get github_url + 'devise_omniauth/config/initializers/omniauth.rb', 'omniauth.rb'
+    begin
+      get github_url + 'lib/devise_omniauth/config/initializers/omniauth.rb', 'omniauth.rb'
+    rescue OpenURI::HTTPError
+      say_wizard "Unable to obtain RSpec example files from the repo"
+    end
   end # config/initializers
+  #
+  # Create Authentications model so that users
+  # can have multiple authentications associated
+  # with one account.
+  #
+  generate :model, 'authentication user_id:integer provider:string uid:string'
+  #
+  # Create Authentications controller 
+  #      actions: index, create and destroy
+  #
+  generate :controller, 'authentications index create destroy'
+  #
+  # User has_many Authentications
+  #
+  inject_into_file "app/models/user.rb", "\nhas_many :authentications", :after => "class User < ActiveRecord::Base"
 end
 
 __END__

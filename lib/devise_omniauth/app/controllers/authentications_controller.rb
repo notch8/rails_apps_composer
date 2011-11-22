42b8960c82ca757fa4c7325fd50abb760aa899d5
Andrew Dixon <dixon@atomicbroadcast.net>
2011-11-22 10:34:24 -0800

    cleaned up debug messages

--- lib/devise_omniauth/app/controllers/authentications_controller.rb
+++ lib/devise_omniauth/app/controllers/authentications_controller.rb
@@ -19,14 +19,12 @@ class AuthenticationsController < ApplicationController
     #     * new user
     #
     if authentication
-      logger.debug "\n\n\t Found an authentication. \n"
       # we found an authentication in the db so
       # we know this is a returning user
       flash[:notice] = "Signed in successfully"
       # sign in and redirect user
       sign_in_and_redirect(:user, authentication.user)
     elsif current_user
-      logger.debug "\n\n\t No authentication but we have current_user. \n"
       # We did not find a matching authentication in the
       # database but current_user is defined. This tells
       # us that a currently logged in user is adding a new
@@ -39,27 +37,21 @@ class AuthenticationsController < ApplicationController
       flash[:notice] = "Authentication successful"
       redirect_to root_url
     else
-      logger.debug "\n\n\t no authentication or current_user. \n"
       # We did not find a matching authentication and there
       # is no current_user. So, we need to create a new User
       # and a new Authentication.       
 
       # new user
       user = User.new
-
       # Build a new authentication
-      logger.debug "\n\n\t build_authentication \n"
       user.build_authentication(omniauth)
-      logger.debug "\n\n\t testapp \n"
       # try to save the user
       if user.save
-        logger.debug "\n\n\t user.save \n"
         # if we pass the authentications redirect the new user
         # to the user's profile page
         flash[:notice] = "Signed in successfully."
         sign_in_and_redirect(:user, user)
       else
-        logger.debug "\n\n\t user.save failed \n"
         # if we did not pass the authentications (e.g. the new
         # user did not have an email address) we redirect to 
         # the new_user_registration_url so that the new user
